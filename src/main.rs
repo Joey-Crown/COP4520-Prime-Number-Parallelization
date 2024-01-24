cad15b76f8266a819c99eee6bb3ccde822983828
Joseph Crown <joe@thecrownfamily.com>
2024-01-23 19:11:26 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use std::fs::File;
+use std::io::Write;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Instant;
+use std::num;
+
+// Function for finding the optimal ranges for splitting up work
+// Explanation: For every number 'x' checked, sqrt(x) operations occur meaning
+// the total work done is equal to the area under the function:
+//   f(n) = sqrt(n)
+// Solve for n segments using Anti-derivative which yields:
+// a_i = (n_i^(2/3) * limit) / 4 (for 8 segments)
+fn find_equal_sections(num_segments: u32, total: u32) -> Vec<u32> {
+    let mut ratios: Vec<u32> = vec![2];
+    for i in 1..(num_segments + 1) {
+        // solve
+        let term = (i as f32).powf(2.0).powf(1.0 / 3.0); // n_i^(2/3)
+        let a = (term / 4.0) * total as f32;
+        ratios.push(a.floor() as u32);
+    }
+    return ratios;
+}
+
+fn is_prime(n: u32) -> bool {
+    for i in 2..=((n as f32).sqrt() as u32) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    let max = 10_000_000;
+    let num_threads = 8;
+    let mut children = vec![];
+    let segment_size = 100_000;
+    let iterations = max / segment_size;
+
+    let start = Instant::now();
+    let ranges: Vec<u32> = find_equal_sections(num_threads, max);
+
+    for i in 0..num_threads {
+        let tx = tx.clone();
+        let thread_start: u32 = ranges[i as usize];
+        let thread_end: u32 = ranges[(i+1) as usize];
+
+        let child = thread::spawn(move || {
+            let mut count = 0;
+            let mut sum: u64 = 0;
+            let mask: u32 = 0b1;
+            for n in thread_start..=thread_end {
+                if n & mask == 1 && is_prime(n as u32) {
+                    count += 1;
+                    sum += n as u64;
+                }
+            }
+            let th_finished_time = start.elapsed();
+            println!("Thread #{i} finished check in {:?}!", th_finished_time);
+            tx.send((count, sum)).unwrap();
+        });
+        children.push(child);
+    }
+
+    let mut total_count = 0;
+    let mut total_sum = 0;
+    for _ in 0..num_threads {
+        let (count, sum) = rx.recv().unwrap();
+        total_count += count;
+        total_sum += sum;
+    }
+
+    for child in children {
+        child.join().unwrap();
+    }
+
+    let duration = start.elapsed();
+
+    println!("Execution Time: {:?}", duration);
+    println!("Total primes found: {total_count}");
+    println!("Sum of all primes: {total_sum}");
+    println!("Print top ten maximum primes found");
+}
